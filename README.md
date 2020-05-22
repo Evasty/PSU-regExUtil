a6a93a4f501cefb778aeae0f8f06025b21b44c46
evak <aknhjy@via.tokyo.jp>
2020-05-22 09:32:49 -0500

    added shiny stuff

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
 # PSU-regExUtil
 
-simple ruby tool to process a csv file with headers (id, text) with a reg ex, 
+simple ruby tool to process a csv file with headers (id, text) with a reg ex,
 
 outputs a headerless csv as follows:
 id, match1, match2 ...
 
 regular expresions can be defined and named in regex.txt and usage is as follows
 regexUtils <csv file path> <regex name> <output file path>
- 
+
 default output will be saved in './' with generated file name
 if no regex name provided a default regex will be used
 
 this module is intended as a solution to extract regions of interest in texts, for example what comes after a certain word which indicates the place of an event.
+
+ADDED:
+black box functions to handle required stuff. just pass it what it needs.
+Fun => processes a CSV as required
+
+Line => returns matches in a line as a hashmap
+JSON => returns JSONobject w matches
+
+note Line and Json receive a regex hashmap {'name' => /regex/}
--- regexUtils.rb
+++ regexUtils.rb
@@ -1,6 +1,6 @@
 #!/bin/ruby
 require 'csv'
-
+require 'json'
 
 =begin
 basic intended usage:
@@ -25,19 +25,31 @@ VdaRe = %r{
   
   
 #sample regex dict, also for testing purposes
-regexDict = {    
+regexDict = {
+  'vereda' => 'en.{1,25}la\svereda\s(.{1,25})',
+  'municipio' => 'en.{1,70}\smunicipio\s(.{1,25})',
+  'dpto' => 'depatamento\s(.{1,25})',
+
   'en la+s vereda+s' => 'en\slas?\sveredas?\s([^\.]+\.)', #sample regex will match from 'en la/s vereda/s' until next dot
-  '-1' => 'en\slas?\sveredas?\s([^\.]+\.)' 
+  '-1' => 'en\slas?\sveredas?\s([^\.]+\.)' ,
+  '-2' => '.*'
+}
+
+#dah
+regexDictLine = {
+  'vereda' => /en.{1,25}la\svereda\s(.{1,25})/i,
+  'municipio' => /en.{1,70}\smunicipio\s(.{1,25})/i,
+  'dpto' => /depatamento\s(.{1,25})/i
 }
 
 #returns matches as array of strings
 def matchLine( line, re )
-  line.scan( /#{re}/ ).flatten()
+  line.scan( /#{re}/i ).flatten()
 end
   
 define_method (:loadRegex){
   |reName|
-  reName.nil? ? regexDict['-1'] : File.foreach( "./regex.txt" ) {
+  reName.nil? ? regexDict['-2'] : File.foreach( "./regex.txt" ) {
     |rxf| 
     if (rxf[0] == '#') 
       next 
@@ -57,10 +69,23 @@ def preProcess( inPath )
   ret.reject! { |r| r.nil?||r[0].nil?||r[1].nil? }
 end
 
+def preProcessBB( inPath )
+  inPath = inPath
+  data = CSV.parse(File.read(inPath.to_s), headers:true )
+  ret = data[data.headers[0]].zip data[data.headers[1]], data[data.headers[2]], data[data.headers[3]], data[data.headers[4]], data[data.headers[5]], data[data.headers[6]], data[data.headers[7]], data[data.headers[8]], data[data.headers[9]], data[data.headers[10]], data[data.headers[11]], data[data.headers[12]]
+  ret.reject! { |r| r.nil?||r[0].nil?}
+  return data,ret
+end
+
 #processes data, returns [id, [match , match]], receives regex as string
 define_method(:applyRx){
   |data , rex |
   data.map! { |x| [x[0] , matchLine( x[1] , rex )] }
+}
+
+define_method(:applyRx2){
+  |data , rex |
+  data.map! { |x|  [matchLine( x , rex )] }
 }
 
 define_method(:array2csv){
@@ -69,32 +94,83 @@ define_method(:array2csv){
 }
 
 define_method(:writeMatches){
-  |outPath, data|
-  outF = File.open( outPath , 'w' )
-  data.each {
-    |rdata|
-    outF.write( array2csv( rdata ) )
-  }
-  outF.close
+  |outPath, data, heads|
+  CSV.open( outPath , 'w' , 
+    :write_headers => true, 
+    :headers => heads ) do |csvF|
+      data.each {
+        |rdata|
+        csvF << rdata 
+      }
+  end
 
 }
 
 help = 
-  "\nuse: regexUtils <csv file path> <regex name> <output file name>\n
-  output path = ./out/<file name>?.csv
-  regex name defaults to '-1', check regex.txt for info
+  "\nuse: regexUtils <csv file path> <output file name?> <regex name?>\n
+  
   <csv file path>: file to process
+  output path = ./out/<file name>?.csv
+  if no regex provided to load defaults to black box functionality (csv + vereda,municipio, departamento )
+  
   "
-#regexUtils <csv file path> <regex name> <output file name> 
+
 define_method (:main){
   if ARGV[0].nil?
     return  puts help
   end
-  outPath = ARGV[2].nil? ? "./#{ARGV[0][0..-5]}#{ARGV[1]}matches.csv" : "./#{ARGV[2]}"
-  rex = loadRegex( ARGV[1] )
-  data = preProcess( ARGV[0] )
-  puts "[in]: #{ARGV[0]} ====>> [out]: #{outPath}"
-  writeMatches(  outPath , applyRx( data , rex ) )
+  outPath = ARGV[1].nil? ? "./#{ARGV[0][0..-5]}#{ARGV[1]}matches.csv" : "./#{ARGV[1]}"
+  if(ARGV[2].nil?)
+    BBres,heads = blackBoxFun(ARGV[0], regexDict)
+    puts "[in]: #{ARGV[0]} ====>> [out]: #{outPath}"
+    writeMatches(  outPath , BBres , heads)
+  else
+    rex = loadRegex( ARGV[2] )
+    data = preProcess( ARGV[0] )
+    puts "[in]: #{ARGV[0]} ====>> [out]: #{outPath}"
+    writeMatches(  outPath , applyRx( data , rex ) )
+  end
+  
 }
+
+#what do i want'??
+#generate output as input w extra cols
+#receives a CSV, applies basic RE and returns first 13 colsa + matches as CSV
+def blackBoxFun(inPath, regexDict)
+ data,save = preProcessBB(inPath)
+ veredas = applyRx2(data['text'].reject!{|x| x.nil?}, regexDict['vereda'] )
+ municipios = applyRx2(data['text'].reject!{|x| x.nil?}, regexDict['municipio'] )
+ dptos = applyRx2(data['text'].reject!{|x| x.nil?}, regexDict['dpto'] )
+ results = []
+ (save.zip veredas,municipios,dptos).each{
+   |x| 
+   results.push(
+      x[0]+ 
+      (x[1][0].empty? ? [""]: [x[1][0][0]]) + #vereda
+      (x[2][0].empty? ? [""]: [x[2][0][0]]) + #municipio
+      (x[3][0].empty? ? [""]: [x[3][0][0]]) ) #dpto
+     
+    }
+  return results , (data.headers[0..12]+['vereda','municipio','departamento'])
+end
+
+
+
+#func that returns json w matches, from map
+define_method(:blackBoxLine){
+  |sampleStr, regexes = regexDictLine|
+  results = {}
+  regexes.each{ |x,y|
+    m = y.match(sampleStr)
+    results.store(x, (m.nil? ? "" : m[1] ) )
+  }
+  return results
+
+}
+define_method(:blackBoxLineJson){
+  |sampleStr, regexes = regexDictLine|
+  return blackBoxLine(sampleStr, regexes).to_json
+}
+
 
 main()
